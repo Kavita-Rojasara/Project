95b0f5249958b8dd9336601db20d3851108c3238
Kavita Rojasara <kavitarojasara0107@gmail.com>
2026-02-23 12:40:38 +0530

    Delete Visual_Product_Search directory

--- Visual_Product_Search/src/utils/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parents[2]
-
-def load_config():
-    config_path = BASE_DIR / "configs/base.yaml"
-    with open(config_path, "r") as f:
-        return yaml.safe_load(f)
--- Visual_Product_Search/src/utils/paths.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from src.utils.config import load_config
-
-BASE_DIR = Path(__file__).resolve().parents[2]
-
-def get_artifact_dir():
-    config = load_config()
-    model_name = config["model"]["backbone"]
-    return BASE_DIR / "artifacts" / model_name
